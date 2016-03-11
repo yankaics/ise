6a4c9ade48eff948d411804d25a9c6d3c83c99f5
bit128 <hongerbo@gmail.com>
2016-03-11 17:39:26 +0800

    完成url存储功能

--- pigeon.py
+++ pigeon.py
@@ -4,13 +4,27 @@ import urllib2
 import MySQLdb
 import MySQLdb.cursors
 from bs4 import BeautifulSoup
+import time
 
 host = 'csdn'
 url = 'http://www.csdn.net'
-
+level = 1
+deep = 3
 #连接数据库
 db = MySQLdb.connect(host='127.0.0.1', user='root', passwd='hong_1987', db='ise', charset='utf8', cursorclass=MySQLdb.cursors.DictCursor)
 cursor = db.cursor()
+
+#生成uniqid
+def uniqid():
+	ts = int(time.time() * 1000000)
+	return str(hex(ts))[2:15]
+
+#判断url唯一性
+def existsHref(href):
+	sql = 'select count(*) as c from t_url_list where url_src=%s'
+	ursor.execute(sql, userId)
+	result = cursor.fetchone()
+	return result['c']
 
 #保存url
 def saveUrl(obj):
@@ -22,9 +36,13 @@ def saveUrl(obj):
 		content = obj.string
 		if content:
 			content = content.strip()
+		else:
+			content = ''
 
-		print href,title,content,"\n"
-
+		sql = 'insert into t_url_list (url_id,url_src,url_title,url_content,url_ctime,url_utime,url_level)'\
+			' values (%s,%s,%s,%s,%s,%s,%s)'
+		cursor.execute(sql, (uniqid(), href, title, content, time.mktime(time.localtime())), 0, level)
+		print 'insert url in ',level,'.'
 
 #爬虫
 def spider(url):
