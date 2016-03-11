1d48bb39891ba6f3c57dfcf72be165c7700edd53
bit128 <hongerbo@gmail.com>
2016-03-11 17:14:51 +0800

    keep dev

--- pigeon.py
+++ pigeon.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 #encoding=utf-8
 import urllib2
+import MySQLdb
+import MySQLdb.cursors
 from bs4 import BeautifulSoup
 
 host = 'csdn'
 url = 'http://www.csdn.net'
 
+#连接数据库
+db = MySQLdb.connect(host='127.0.0.1', user='root', passwd='hong_1987', db='ise', charset='utf8', cursorclass=MySQLdb.cursors.DictCursor)
+cursor = db.cursor()
+
+#保存url
+def saveUrl(obj):
+	href = obj['href']
+	if href[0:4] = 'http':
+		title = ''
+		if obj.has_key('title'):
+			title = obj['title']
+		content = obj.string
+
+		print href,title,content,"\n"
+
+
+#爬虫
 def spider(url):
 	#数据报头
 	header = {}
@@ -18,13 +37,14 @@ def spider(url):
 	#解析html文档
 	parseHtml(result)
 
+#html解析器
 def parseHtml(content):
 	soup = BeautifulSoup(content)
 	alist = soup.select('a')
 	
 	for item in alist:
 		if item.has_key('href'):
-			print item['href']
+			saveUrl(obj)
 
 
 spider(url)
